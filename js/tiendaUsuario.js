630276e42e3936abb71cb9403ec5c38c2f27b227
Juan Gerardo <jmendez1025@cue.edu.co>
2022-06-20 13:46:37 -0500

    90% tienda

--- js/tiendaUsuario.js
+++ js/tiendaUsuario.js
@@ -8,6 +8,7 @@ const printProductos = () => {
         let div = document.createElement("DIV");
         div.innerHTML = "";
         div.setAttribute("class", "container-productos")
+        document.getElementById("titulo").innerHTML="TIENDA";
         for (let i = 0; i < 5; i++) {
             let li = document.createElement("LI");
             switch (i) {
@@ -57,12 +58,27 @@ const getProductos = async () => {
     console.log(arrayProductos);
     printProductos();
 }
+const putProductoAPI = (productoId, cantidad) => {
+    try {
+        fetch(urlApi + "/" + productoId, {
+            method: 'PUT',
+            body: JSON.stringify({
+                cantidad: cantidad
+            }),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+
+    } catch (error) {
+        console.log(`No se pudo actualizar la información del producto con el ID:${productoId}`);
+    }
+
+}
 //Lleno el arreglo con la API
 getProductos();
-
+//Ocultar divs  del container
 const ocultarDivs = (button) => {
-
-
     button.addEventListener("click", () => {
         document.getElementById("titulo").innerHTML = "BOLSA";
         arrayProductos.forEach((object, index) => {
@@ -70,6 +86,7 @@ const ocultarDivs = (button) => {
         })
     })
 }
+//Seccion de vista previa
 const editarProducto = (button) => {
     button.addEventListener("click", () => {
         let idButton = button.id;
@@ -102,12 +119,6 @@ const editarProducto = (button) => {
                 case 2:
                     li.innerHTML = `Precio:${arrayProductos[idButton - 1].precio}$`;
                     div.appendChild(li);
-                    let p = document.createElement("P");
-                    p.innerHTML = "Producto actualizado correctamente";
-                    p.setAttribute("id", "p-correcto");
-                    p.style.color = "green";
-                    p.style.display = "none";
-                    p.style.fontSize = "16px"
                     break;
                 case 3:
                     let botonCarro = document.createElement("BUTTON");
@@ -136,6 +147,96 @@ const editarProducto = (button) => {
 
     )
 };
+
+//constructor de inputs + p 
+const inputList = (p, input, text, type, div, placeholder) => {
+    p.innerHTML = text;
+    div.appendChild(p);
+    input.setAttribute("type", type);
+    input.setAttribute("placeholder", placeholder);
+    input.setAttribute("class","input");
+    div.appendChild(input);
+}
+// imprimir la facturacion
+const printFacturacion = () => {
+    containerProductos.innerHTML = "";
+    let titulo = document.getElementById("titulo");
+    titulo.innerHTML = "FACTURACIÓN";
+    let divFormulario = document.createElement("DIV");
+    divFormulario.setAttribute("class", "container-formulario");
+    let divInputs = document.createElement("DIV");
+    divInputs.setAttribute("class", "container-inputs");
+    for (let i = 0; i < 7; i++) {
+        let p = document.createElement("P");
+        let input = document.createElement("INPUT");
+        switch (i) {
+            case 0:
+                inputList(p, input, "Nombre completo", "text", divInputs, "Nombre completo");
+                break;
+            case 1:
+                inputList(p, input, "Numero de celular", "number", divInputs, "Numero de celular");
+                break;
+            case 2:
+                inputList(p, input, "No. de documento de identidad", "number", divInputs, "No. de documento de identidad");
+                break;
+            case 2:
+                inputList(p, input, "Ciudad", "text", divInputs, "Ciudad");
+                break;
+            case 3:
+                inputList(p, input, "Dirección de entrega", "text", divInputs, "Dirección de entrega");
+                break;
+            case 4:
+                inputList(p, input, "Número de tarjeta", "text", divInputs, "Número de tarjeta");
+                break;
+            case 5:
+                let buttonConfirmar = document.createElement("BUTTON");
+                buttonConfirmar.innerHTML = "Confirmar";
+                divInputs.appendChild(buttonConfirmar);
+                buttonConfirmar.addEventListener("click",()=>{
+                    if(confirm("Estás seguro de finalizar la compra")){
+                        finalizarCompra();
+                    } 
+                })
+                break;
+        }
+    }
+    let img = document.createElement("IMG");
+    img.setAttribute("src", "https://i.postimg.cc/02Y1WJcb/i.png");
+    divFormulario.appendChild(divInputs);
+    divFormulario.appendChild(img);
+    containerProductos.appendChild(divFormulario);
+
+}
+const clearInputs = () => {
+    for (let i = 0; i < 5; i++) {
+        document.querySelectorAll(".input")[i].value = "";
+    }
+}
+const comprobarDatos = () => {
+    let contador = 0;
+    for (let i = 0; i < 5; i++) {
+        if (document.querySelectorAll(".input")[i].value == "") contador++;
+    }
+    if (contador > 0) {
+        return false;
+    } else return true;
+}
+const finalizarCompra = ()=>{
+    if(comprobarDatos()){
+        let arregloID = arrayCompras.filter(object=>object.id);
+        console.log(arregloID);
+        for(let i in arregloID){
+            setClassLists();
+            putProductoAPI(arregloID[i].id,arrayProductos[i].cantidad-1);
+            clearInputs();
+            arrayCompras.splice(0,arrayCompras.length);
+            document.getElementById("contador-compras").innerHTML = arrayCompras.length;
+        }
+        
+    }else{
+        setClassLists();
+    }
+}
 let contadorCompras = document.getElementById("contador-compras");
 arrayCompras = [];
 const botonCarroFunction = (id) => {
@@ -144,15 +245,56 @@ const botonCarroFunction = (id) => {
 }
 let contadorComprasContainer = document.getElementById("contador-compras-container");
 ocultarDivs(contadorComprasContainer);
+const setClassLists = () => {
+    for (let i = 0; i < 4; i++) {
+        if (document.querySelectorAll(".input")[i].value == "") {
+            document.querySelectorAll(".input")[i].classList.add("error");
+        } else {
+            document.querySelectorAll(".input")[i].classList.remove("error");
+        }
+    }
+}
 
-
+const compraExitosa = ()=>{
+    let divPadre = document.getElementById("container-productos");
+    divPadre.innerHTML="";
+    let div = document.createElement("DIV");
+    div.setAttribute("class","container-exitoso");
+    let img = document.createElement("IMG");
+    div.appendChild(img);
+    let p = document.createElement("P");
+    p.innerHTML="Compra exitosa!";
+    div.appendChild(p);
+    let p2= document.createElement("P");
+    p2.innerHTML="Detalles de la compra:";
+    div.appendChild(p2);
+    arrayCompras.forEach(objec=>{
+        
+    })
+}
+const deleteProducto = (button) => {
+    button.addEventListener("click", () => {
+        let id = button.id;
+        id = id.slice(-1);
+        id = parseInt(id);
+        console.log(id);
+        arrayCompras.splice(id, 1);
+        console.log(arrayCompras);
+        contadorCompras.innerHTML = arrayCompras.length;
+        let divPadre = document.getElementById("div-bolsa");
+        printBolsa();
+    })
+}
 const printBolsa = () => {
     if (arrayCompras.length > 0) {
         const divBolsa = document.createElement("DIV");
+        divBolsa.setAttribute("id", "div-bolsa");
         divBolsa.setAttribute("class", "div-bolsa");
-        arrayCompras.forEach(object => {
+        containerProductos.innerHTML = "";
+        arrayCompras.forEach((object, index) => {
             let div = document.createElement("DIV");
             div.setAttribute("class", "bolsa-item");
+            div.setAttribute("id", `container-facturacion-${index}`);
             for (let i = 0; i < 3; i++) {
                 switch (i) {
                     case 0:
@@ -164,6 +306,7 @@ const printBolsa = () => {
                     case 1:
                         let divItems = document.createElement("DIV");
                         divItems.setAttribute("class", "vista-previa-");
+
                         divItems.innerHTML = "";
                         for (let j = 0; j < 3; j++) {
                             let li = document.createElement("LI");
@@ -176,12 +319,7 @@ const printBolsa = () => {
                                 divItems.appendChild(li);
                             }
                             else {
-                                let totalCompra = arrayCompras.reduce((total, acum) => {
-                                    acum.precio = parseInt(acum.precio);
-                                    return total + acum.precio;
-                                }, 0)
-                                console.log(totalCompra);
-                                li.innerHTML = `${totalCompra}$`;
+                                li.innerHTML = `Precio ${object.precio}$`;
                                 divItems.appendChild(li);
                             }
                         }
@@ -191,23 +329,47 @@ const printBolsa = () => {
                         let imgX = document.createElement("IMG");
                         imgX.setAttribute("src", "https://static.vecteezy.com/system/resources/previews/001/200/173/original/x-png.png");
                         imgX.setAttribute("class", "x-icon");
+                        imgX.setAttribute("id", `facturacion-${index}`);
+                        deleteProducto(imgX);
                         div.appendChild(imgX);
                 }
             }
             divBolsa.appendChild(div);
         })
-        let backButton=document.createElement("BUTTON");
-        backButton.innerHTML="Volver";
-        backButton.addEventListener("click",()=>{
+        let totalCompra = arrayCompras.reduce((total, acum) => {
+            acum.precio = parseInt(acum.precio);
+            return total + acum.precio;
+        }, 0)
+        let p = document.createElement("P");
+        p.innerHTML = `Precio total: ${totalCompra}$`;
+        p.setAttribute("id", "totalCompra");
+        divBolsa.appendChild(p);
+        let finalizarCompra = document.createElement("BUTTON");
+        finalizarCompra.innerHTML = "Finalizar compra";
+        finalizarCompra.setAttribute("class", "botones");
+        divBolsa.appendChild(finalizarCompra);
+        let backButton = document.createElement("BUTTON");
+        backButton.innerHTML = "Volver";
+        backButton.setAttribute("class", "botones");
+        finalizarCompra.addEventListener("click", () => {
+            printFacturacion();
+        })
+        backButton.addEventListener("click", () => {
+            document.getElementById("titulo").innerHTML = "TIENDA";
             printProductos();
         })
         divBolsa.appendChild(backButton);
         containerProductos.appendChild(divBolsa);
     } else {
-        alert("Aún no has añadido nada al carrito")
+        alert("Aquí no hay nada");
+        printProductos()
+        
     }
 
 }
+
+
 contadorComprasContainer.addEventListener("click", () => {
     printBolsa();
 });
+
